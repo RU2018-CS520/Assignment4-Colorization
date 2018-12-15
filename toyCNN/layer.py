3fca377e3041818ff5835bff5ac0be27124597c2
yhz012 <603907639@qq.com>
2018-12-16 07:58:10 +0800

    debug

--- toyCNN/layer.py
+++ toyCNN/layer.py
@@ -4,7 +4,11 @@ from frame import tools
 from frame import funcs
 
 class layer(object):
+	#general layer frame, DO NOT use it!
 	def __init__(self, iSize, oSize):
+		#tuple iSize = (ix, iy, iz): input shape
+		#tuple oSize = (ox, oy ,oz): output shape
+
 		self.ix, self.iy, self.iz = iSize
 		self.ox, self.oy, self.oz = oSize
 		self.type = ''
@@ -15,21 +19,40 @@ class layer(object):
 	def __str__(self):
 		return 'layer: ' + self.type + ' layer: ' + str((self.ix, self.iy, self.iz)) + ' -> ' + str((self.ox, self.oy, self.oz))
 
+	#check if oSize matches iSize and other configs
 	def validCheck(self):
 		pass
 
+	#forward input data
 	def forward(self, iData):
+		#in:
+		#np.ndarray x with ndim = 3: input data point
+		#out:
+		#np.ndarray x with ndim = 3: output data point
 		pass
 
+	#backward modified loss
 	def backward(self, loss):
+		#in:
+		#np.ndarray loss with ndim = 3: loss.derivative in previous layer
+		#out:
+		#np.ndarray loss with ndim = 3: loss.derivative in this layer
 		pass
 
+	#use modified loss update kernel
 	def update(self, learnRate):
+		#float learnRate in [0: 1]: update step size
 		pass
 
 
 class conv(layer):
+	#convolutional layer
 	def __init__(self, iSize, oSize, kernelNum, padSize, stepLen, kernelSize):
+		#uint kernelNum: number of kernels
+		#uint padSize: outer pad size
+		#uint stepLen: kernel moving step size
+		#tuple kernelSize = (x, y): kernel shape
+
 		super(conv, self).__init__(iSize, oSize)
 		self.type = 'c'
 
@@ -41,6 +64,7 @@ class conv(layer):
 
 		self.validCheck()
 
+		#init conv kernel
 		self.kernel = np.random.rand(self.kernelNum , self.iz, self.kernelSize[0], self.kernelSize[1]).astype(np.float16)
 		return
 
@@ -57,9 +81,13 @@ class conv(layer):
 
 	def forward(self, iData):
 		self.iData = iData
+		#pad iData
 		tData = tools.pad(self.ix, self.iy, self.iz, self.padSize, iData)
 
+		#init oData
 		res = np.empty((self.oz, self.ox, self.oy), dtype = np.float16)
+
+		#convolution
 		for row in range(self.ox):
 			t = row * self.stepLen
 			b = t + self.kernelSize[0]
@@ -77,10 +105,15 @@ class conv(layer):
 		tz = self.iz
 		tx = self.ix+2*self.padSize
 		ty = self.iy+2*self.padSize
+
+		#init oLoss
 		res = np.empty((tz, self.oz, tx, ty), dtype = np.float16)
 
+		#transpose iAxis and oAxis
+		#transpose oAxis to the last axis to match iLoss
 		rKernel = np.transpose(self.kernel, (1, 2, 3, 0))
 
+		#de-convolution
 		for row in range(self.ox):
 			t = row * self.stepLen
 			b = t + self.kernelSize[0]
@@ -88,21 +121,26 @@ class conv(layer):
 				l = col * self.stepLen
 				r = l + self.kernelSize[1]
 
+				#transpose oAxis back
 				res[:, :, t: b, l: r] = np.transpose((loss[:, row, col] * rKernel), (0, 3, 1, 2))
 
+		#de-pad
 		if self.padSize == 0:
 			self.loss = res
 		else:
 			self.loss = res[:, :, self.padSize: self.padSize+self.ix, self.padSize: self.padSize+self.iy]
 
+		#sum oAxis to backward
 		res = np.sum(self.loss, axis = 1)
 		return res
 
 
 	def update(self, learnRate):
+		#pad iData and loss
 		tData = tools.pad(self.ix, self.iy, self.iz, self.padSize, self.iData)
-		loss = np.transpose(self.loss, (1, 0, 2, 3))
+		loss = tools.pad4(self.ix, self.iy, self.iz, self.oz, self.padSize, np.transpose(self.loss, (1, 0, 2, 3)))
 
+		#stack convlution
 		res = np.zeros_like(self.kernel, dtype = np.float16)
 		for row in range(self.ox):
 			t = row * self.stepLen
@@ -111,23 +149,30 @@ class conv(layer):
 				l = col * self.stepLen
 				r = l + self.kernelSize[1]
 
-				res = res + tData[:, t: b, l: r] * loss
+				res = res + tData[:, t: b, l: r] * loss[:, :, t: b, l: r]
 
+		#average
 		res = res / (self.ox * self.oy)
+
+		#update
 		self.kernel = self.kernel - learnRate * res
 		return
 
 
 
 class active(layer):
+	#activation layer
 	def __init__(self, iSize, oSize, func):
+		#funcs func: active function
+
 		super(active, self).__init__(iSize, oSize)
 		self.type = 'a'
 		self.func = func
 
 		self.validCheck()
 
-		self.kernel = np.random.rand(1, self.iz, self.ix, self.iy).astype(np.float16)
+		#init bias
+		self.kernel = np.random.rand(self.iz, self.ix, self.iy).astype(np.float16)
 		return
 
 
@@ -149,17 +194,24 @@ class active(layer):
 
 	def backward(self, loss):
 		res = loss * self.func.derivative(self.iData, self.oData, self.kernel)
+
+		#backward w = 1, bias w = bias
 		self.loss = self.kernel * res
 		return res
 
 
 	def update(self, learnRate):
+
+		#bias o = 1
 		self.kernel = self.kernel - learnRate * self.loss
 		return
 
 
 class pool(layer):
 	def __init__(self, iSize, oSize, stepLen, func):
+		#uint stepLen: kernel moving step size
+		#funcs func: pool function
+
 		super(pool, self).__init__(iSize, oSize)
 		self.type = 'p'
 		self.stepLen = stepLen
@@ -183,6 +235,7 @@ class pool(layer):
 	def forward(self, iData):
 		self.iData = iData
 
+		#convolution
 		res = np.empty((self.oz, self.ox, self.oy), dtype = np.float16)
 		for row in range(self.ox):
 			t = row * self.stepLen
@@ -200,6 +253,7 @@ class pool(layer):
 	def backward(self, loss):
 		res = np.empty((self.iz, self.ix, self.iy), dtype = np.float16)
 
+		#de-convolution
 		for row in range(self.ox):
 			t = row * self.stepLen
 			b = t + self.kernelSize[0]
@@ -213,12 +267,15 @@ class pool(layer):
 
 
 	def update(self, learnRate):
+		#nothing to do
 		return
 
 
 
 class fullC(layer):
 	def __init__(self, iSize, oSize, kernelNum):
+		#uint kernelNum: number of outputs
+
 		super(fullC, self).__init__(iSize, oSize)
 		self.type = 'f'
 		self.kernelNum = kernelNum
@@ -247,11 +304,17 @@ class fullC(layer):
 
 
 	def backward(self, loss):
+
+		#transpose iAxis and oAxis
 		rKernel = np.transpose(self.kernel, (1, 0, 2, 3))
 		self.loss = loss * rKernel
+
+		#sum oAxis to backward
 		res = np.sum(self.loss, axis = 1)
 		return res
 
 	def update(self, learnRate):
+
+		#transpose oAxis and iAxis to match iData
 		self.kernel = self.kernel - learnRate * self.loss.transpose(1,0,2,3) * self.iData
 		return
