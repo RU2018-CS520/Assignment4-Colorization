f1ae9e638dd92d7c8194ce6b5cec1415abdbba90
yhz012 <603907639@qq.com>
2018-12-15 14:51:17 +0800

    更新 tools.py

--- toyCNN/frame/tools.py
+++ toyCNN/frame/tools.py
@@ -6,3 +6,13 @@ def pad(ix, iy, iz, padSize, iData):
 	res = np.zeros((iz, ix+2*padSize, iy+2*padSize), dtype = np.float16)
 	res[:, padSize: padSize+ix, padSize: padSize+iy] = iData
 	return res
+
+
+def setNpSeed(seed = 6983):
+	np.random.seed = seed
+	return
+
+
+class lossFunc(object):
+	def norm2(o, y):
+		return np.square(o - y)
